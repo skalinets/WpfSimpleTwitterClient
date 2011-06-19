d08e228328d8928c34a2fa70ef9984ce5eaec5eb
skalinets <kalinets@gmail.com>
2011-06-20 00:23:12 +0300

    initial commit

---
+++ Client/TweetsViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using LinqToTwitter;
+
+namespace Client
+{
+    public class TweetsViewModel
+    {
+        private readonly ObservableCollection<Tweet> tweets = new ObservableCollection<Tweet>();
+
+        public ObservableCollection<Tweet> Tweets
+        {
+            get { return tweets; }
+        }
+
+        static TweetsViewModel()
+        {
+            Mapper.CreateMap<Status, Tweet>();
+        }
+
+
+        public void Load()
+        {
+            var synchronizationContext = SynchronizationContext.Current;
+            
+            Action<Task<IEnumerable<Tweet>>> continuationFunction = t => t.Result.ToList().ForEach(e => { } /*tweets.Add*/);
+            Task<IEnumerable<Tweet>>.Factory.StartNew(() =>
+                                                          {
+                                                              var twitterCtx = new TwitterContext();
+                                                              return from tweet in twitterCtx.Status where tweet.Type == StatusType.Public select Mapper.Map<Status, Tweet>(tweet);
+                                                          })
+                                                          .ContinueWith(t => synchronizationContext.Post(state => continuationFunction(t), null));
+        }
+    }
+
+    public class Tweet
+    {
+        public string UserName { get; set; }
+        public string UserProfileImageUrl { get; set; }
+        public string Text { get; set; }
+    }
+
+
+}
---
+++ Tests/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LinqToTwitter;
+using Xunit;
+
+namespace Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void FactMethodName()
+        {
+            var twitterCtx = new TwitterContext();
+
+            var publicTweets =
+                from tweet in twitterCtx.Status
+                where tweet.Type == StatusType.Public
+                select tweet;
+
+            var t = from publicTweet in publicTweets 
+                let user = publicTweet.User 
+            select new {user.ProfileImageUrl, user.Name, publicTweet.Text};
+
+            t.ToList().ForEach(Console.WriteLine);
+//
+//            publicTweets.ToList().ForEach(
+//                tweet => Console.WriteLine(
+//                    "User Name: {0}, Tweet: {1}",
+//                    tweet.User.Name,
+//                    tweet.Text));
+        }
+    }
+}
